94f4c98beb09be921e4919da66eb1fe143519c60
t-harada <harada@platinum-egg.com>
2018-05-03 15:29:47 +0900

    カメラテスト＆＆パーティクルテスト

---
+++ flying_puzzle/Assets/resource_load.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resource_load : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Asset内にあるresourcesフォルダから、Textureを読み込む。
+		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		Renderer rend = go.GetComponent<Renderer>();
+		rend.material.mainTexture = Resources.Load("glass") as Texture;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
